69c5babe34eb7844985b52acf9827ee1811ce54c
liufq <popsay@163.com>
2024-12-25 19:29:19 +0800

    1.init commit

---
+++ include/communication/communication/msg/state_estimator.hpp
@@ -0,0 +1,12 @@
+// generated from rosidl_generator_cpp/resource/idl.hpp.em
+// generated code does not contain a copyright notice
+
+#ifndef COMMUNICATION__MSG__STATE_ESTIMATOR_HPP_
+#define COMMUNICATION__MSG__STATE_ESTIMATOR_HPP_
+
+#include "communication/msg/detail/state_estimator__struct.hpp"
+#include "communication/msg/detail/state_estimator__builder.hpp"
+#include "communication/msg/detail/state_estimator__traits.hpp"
+#include "communication/msg/detail/state_estimator__type_support.hpp"
+
+#endif  // COMMUNICATION__MSG__STATE_ESTIMATOR_HPP_
